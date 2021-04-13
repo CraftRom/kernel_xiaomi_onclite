cf12aa345f1d7225f9e28837672d85b1a7e26346
Sultan Alsawaf <sultan@kerneltoast.com>
2021-04-13 15:42:06 +0300

    proc: cmdline: Patch SafetyNet flags

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -8,7 +8,7 @@ static char new_command_line[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", saved_command_line);
+	seq_printf(m, "%s\n", new_command_line);
 	return 0;
 }
 
@@ -24,44 +24,38 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
-#if 1
-static void replace_flag(char *cmd, const char *flag, const char *flag_new)
+static void patch_flag(char *cmd, const char *flag, const char *val)
 {
-	char *start_addr, *end_addr;
+	size_t flag_len, val_len;
+	char *start, *end;
 
-	/* Ensure all instances of a flag are replaced */
-	while ((start_addr = strstr(cmd, flag))) {
-		end_addr = strchr(start_addr, ' ');
-		if (end_addr)
-			memcpy(start_addr, flag_new, strlen(flag));
-		else
-			*(start_addr - 1) = '\0';
-	}
+	start = strstr(cmd, flag);
+	if (!start)
+		return;
+
+	flag_len = strlen(flag);
+	val_len = strlen(val);
+	end = start + flag_len + strcspn(start + flag_len, " ");
+	memmove(start + flag_len + val_len, end, strlen(end) + 1);
+	memcpy(start + flag_len, val, val_len);
 }
 
-static void replace_safetynet_flags(char *cmd)
+static void patch_safetynet_flags(char *cmd)
 {
-	// WARNING: be aware that you can't replace shorter string with longer ones in the function called here...
-	replace_flag(cmd, "androidboot.vbmeta.device_state=unlocked",
-			  "androidboot.vbmeta.device_state=locked  ");
-	replace_flag(cmd, "androidboot.enable_dm_verity=0",
-			  "androidboot.enable_dm_verity=1");
-	replace_flag(cmd, "androidboot.secboot=disabled",
-			  "androidboot.secboot=enabled ");
-	replace_flag(cmd, "androidboot.verifiedbootstate=orange",
-			  "androidboot.verifiedbootstate=green ");
+	patch_flag(cmd, "androidboot.flash.locked=", "1");
+	patch_flag(cmd, "androidboot.verifiedbootstate=", "green");
+	patch_flag(cmd, "androidboot.veritymode=", "enforcing");
 }
-#endif
 
 static int __init proc_cmdline_init(void)
 {
 	strcpy(new_command_line, saved_command_line);
 
 	/*
-	 * Remove/replace various flags from command line seen by userspace in order to
-	 * pass SafetyNet CTS check.
+	 * Patch various flags from command line seen by userspace in order to
+	 * pass SafetyNet checks.
 	 */
-	replace_safetynet_flags(new_command_line);
+	patch_safetynet_flags(new_command_line);
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
